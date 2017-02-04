1204ac9eb617607903d163e1cb61fac0f7d935d5
yury <orlangure@gmail.com>
2017-02-04 19:24:40 +0200

    Update javadoc

--- maybach/src/main/java/com/alheap/maybach/Event.java
+++ maybach/src/main/java/com/alheap/maybach/Event.java
@@ -2,16 +2,33 @@ package com.alheap.maybach;
 
 /**
  * Created by ALENHEART.APPS (ALHEAP)
+ * <p>
+ * This class represents an event that happens inside the application. Every event has a name
+ * that identifies it. That name is used to trigger the event at runtime.
  *
  * @author Alex Kamenkov
  */
-
 public abstract class Event<Data> {
+    /**
+     * A name of the event, used to identify and trigger it.
+     */
     protected final String name;
 
-    protected Event(String name) {
+    /**
+     * Create a new Event with a provided name
+     *
+     * @param name a name of the event
+     */
+    protected Event(final String name) {
         this.name = name;
     }
 
+    /**
+     * This method is called when the event is triggered. It accepts an argument of a type that
+     * was defined as a type of the event.
+     *
+     * @param data an object of a type that was defined at the event's creation. It should contain
+     *             the data that the event needs to process and perform some actions on.
+     */
     protected abstract void onEvent(Data data);
 }
--- maybach/src/main/java/com/alheap/maybach/Maybach.java
+++ maybach/src/main/java/com/alheap/maybach/Maybach.java
@@ -3,66 +3,81 @@ package com.alheap.maybach;
 import android.util.Log;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ALENHEART.APPS (ALHEAP)
+ * <p>
+ * This class provides an ability to transfer data across activities, fragments, services and
+ * broadcast receivers inside an application.
+ * <p>
+ * Objects that are supposed to wait for something to happen somewhere else inside the application,
+ * sigh up for the events they are interested in, and wait for them to happen asynchronously.
+ * Objects that know when such events happen, trigger them by name passing along the relevant data.
+ * Objects that are no longer interested in the events that they signed up for, can
+ * unregister the event and stop waiting for it.
  *
  * @author Alex Kamenkov
  */
-
-/**
- * This class have some cool and easy to use features
- * to connect your activities, fragments, broadcast receivers and
- * services by sign up for events in one side and trigger them on another.
- */
 public class Maybach {
+    /**
+     * A single instance of this class
+     */
     private static Maybach sInstance = new Maybach();
-    private HashMap<Object, HashMap<String, Event>> mReferenceMap = new HashMap<>();
+
+    /**
+     * A map that holds references to signed up objects ant the events that they are signed up for
+     */
+    private Map<Object, Map<String, Event>> mReferenceMap = new HashMap<>();
+
+    /**
+     * This tag is used for debugging
+     */
     private final String TAG = getClass().getSimpleName();
 
     /**
-     * Getting the instance to Maybach.
+     * Get the only instance of this class
      */
     public static Maybach get() {
         return sInstance;
     }
 
     /**
-     * Sign up your reference object (activity for example), to event.
-     * Each event has its unique name, by this name we are going to trigger it later.
+     * Add a new event and associate it with a provided object reference (activity, fragment...).
+     * The event should have a unique name by which it can be triggered later.
      *
-     * @param reference object, reference to activity for example.
-     * @param event     the event, by calling his name we can trigger it.
+     * @param reference an object that is associated with an event (activity, fragment, etc)
+     * @param event     an event that may be called later by its unique name
      */
     public void signUpForEvent(Object reference, Event event) {
         if (mReferenceMap.containsKey(reference)) {
             mReferenceMap.get(reference).put(event.name, event);
         } else {
-            HashMap<String, Event> mapOfEvents = new HashMap<>();
+            Map<String, Event> mapOfEvents = new HashMap<>();
             mapOfEvents.put(event.name, event);
             mReferenceMap.put(reference, mapOfEvents);
         }
     }
 
     /**
-     * Unregister an event from the reference.
+     * Remove an event subscription for a provided object reference.
      *
-     * @param reference object, reference to activity for example.
-     * @param event     the event that should be unregistered.
+     * @param reference an object that is associated with an event (activity, fragment, etc)
+     * @param event     an event that is associated with a provided object
      */
     public void unregisterEvent(Object reference, Event event) {
         unregisterEvent(reference, event.name);
     }
 
     /**
-     * Unregister an event (by name) from the reference.
+     * Remove an event subscription for a provided object reference given the event's name.
      *
-     * @param reference object, reference to activity for example.
-     * @param eventName event with this name should be unregistered.
+     * @param reference an object that is associated with an event (activity, fragment, etc)
+     * @param eventName an event that is associated with a provided object
      */
     public void unregisterEvent(Object reference, String eventName) {
         if (mReferenceMap.containsKey(reference)) {
-            HashMap<String, Event> mapOfEvents = mReferenceMap.get(reference);
+            Map<String, Event> mapOfEvents = mReferenceMap.get(reference);
             if (mapOfEvents.containsKey(eventName)) {
                 mapOfEvents.remove(eventName);
                 if (mapOfEvents.isEmpty()) {
@@ -73,13 +88,14 @@ public class Maybach {
     }
 
     /**
-     * Trigger an event by its name.
+     * Trigger an event by its name. Objects associated with the event will execute their event's
+     * onEvent methods, receiving a provided data object as an argument.
      *
-     * @param eventName event with this name should be triggered.
-     * @param data      object that should be passed to the event.
+     * @param eventName a unique name of an event that should be triggered
+     * @param data      a data object that should be passed to an event
      */
     public void triggerEvent(String eventName, Object data) {
-        for (HashMap<String, Event> mapOfEvents : mReferenceMap.values()) {
+        for (Map<String, Event> mapOfEvents : mReferenceMap.values()) {
             if (mapOfEvents.containsKey(eventName)) {
                 try {
                     //noinspection unchecked
@@ -92,9 +108,9 @@ public class Maybach {
     }
 
     /**
-     * Unregister all events from the reference.
+     * Remove all event subscriptions for a provided object reference (activity, fragment, etc).
      *
-     * @param reference object, reference to activity for example.
+     * @param reference an object that its subscriptions should be removed (activity, fragment, etc)
      */
     public void unregisterClassEvents(Object reference) {
         mReferenceMap.remove(reference);
